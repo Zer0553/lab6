efd9bd9beb683606ae986b4a97dee6bd88eb2ce3
Zer0553 <124603918+Zer0553@users.noreply.github.com>
2023-05-16 10:45:57 +0400

    Update lab6_2.py

--- lab6_2.py
+++ lab6_2.py
@@ -1,6 +1,6 @@
 # Дана квадратная матрица. Сформировать все возможные варианты данной матрицы путем
 # перестановки строк и столбцов, в которых диагональный элемент равен нулю.
-# + сумма элементов сторки с диагональным элементом равным нулю не должна быть бльше размера матрицы уноженного на 5
+# + сумма элементов сторки с диагональным элементом равным нулю не должна быть больше размера матрицы уноженного на 5
 # + в случайно сгенерированном матрице должен быть хотя бы 1 нулевой диагональный элемент
 
 
@@ -9,18 +9,11 @@ import copy
 
 
 def check_1(n):
-    null = False
-    k = 0
-    matrix = [[random.randint(0, 10) for i in range(n)] for j in range(n)]
-    while k != n:
-        if matrix[k][k] == 0 or matrix[k][n - 1 - k] == 0:
-            null = True
-        k += 1
-    if null:
-        return matrix
-    else:
-        check_1(n)
-
+    while True:
+        matrix = [[random.randint(0, 10) for i in range(n)] for j in range(n)]
+        for i in range(n):
+            if matrix[i][i] == 0 or matrix[i][n - 1 - i] == 0:
+                return matrix
 
 def number_check(n):
     while True:
@@ -44,8 +37,8 @@ def print_matrix(matrix):
     print()
 
 
-def check_2(matrix, row, column):
-    if sum(matrix[row][column]) > len(matrix) * 5:
+def check_2(matrix, row):
+    if sum(matrix[row]) > len(matrix) * 5:
         return False
     else:
         return True
@@ -55,7 +48,7 @@ def F_rec(matrix, row, column, count, n, exist=[[], []]):
     if row == len(matrix):
         return
     F_rec(matrix, row + 1, column + 1, count, n)
-    if check_2(matrix, row, column):
+    if check_2(matrix, row):
         if matrix[row][row] == 0 and row not in exist[0]:
             exist[0].append(row)
             new_matrix = copy.deepcopy(matrix)
@@ -77,17 +70,16 @@ def F_rec(matrix, row, column, count, n, exist=[[], []]):
 
 def F_iter(matrix):
     count = 0
-    exist=[[], []]
-    stack = []
+    exist = [[], []]
+    stack = [(copy.deepcopy(matrix), 0, 0)]
     k = n - 1
     while stack:
-
         matrix, row, column = stack.pop()
         if row == len(matrix):
             count += 1
             continue
         stack.append((matrix, row + 1, column + 1))
-        if check_2(matrix, row, column):
+        if check_2(matrix, row):
             if matrix[row][row] == 0 and row not in exist[0]:
                 exist[0].append(row)
                 new_matrix = copy.deepcopy(matrix)
@@ -102,20 +94,26 @@ def F_iter(matrix):
                     new_matrix[row][i], new_matrix[k - i][k - column] = new_matrix[k - i][k - column],  new_matrix[row][i]
                 print_matrix(new_matrix)
                 stack.append((matrix, row + 1, column + 1))
-        stack.append((matrix, row + 1, column + 1))
+        else:
+            if matrix[row][row] == 0 and row not in exist[0]:
+                print('сумма элементов строки ' + str(row) + ' оказалась больше размера матрицы * 5')
+            if matrix[row][k - row] == 0 and row != len(matrix) // 2 and row not in exist[1]:
+                print('сумма элементов строки ' + str(row) + ' оказалась больше размера матрицы * 5')
     print('Общее количество вариантов: ', count - 1)
 
 
 count = [0]
 n = number_check('Размер матрицы: ')
-matrix = check_1(n)
+if input('Напишить 1 для запуска тестовой матрицы, 2 для случайной: ') == '1':
+    matrix = [[10, 9, 0], [3, 0, 3], [0, 2, 5]]
+else:
+    matrix = check_1(n)
 print('Начальная матрица:')
 print_matrix(matrix)
 
 print('Рекурсивный перебор возможных вариантов.')
 F_rec(matrix, 0, 0, count, n - 1)
-print('Общее количество вариантов: ', count[0] - 1)
+print('Общее количество вариантов: ', count[0])
 
 print('Итеративный перебор возможных вариантов.')
 F_iter(matrix)
-print('Проверяемый массив не подошел под условия.')
